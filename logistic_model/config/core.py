783620268c3f3d9602bc7c47a18f317bdf04e3a5
dsydykov <damirbek.sydykov@opitz-consulting.de>
2022-09-22 16:26:13 +0200

    update core, added config functions

--- logistic_model/config/core.py
+++ logistic_model/config/core.py
@@ -13,12 +13,11 @@ CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
 
-class AppConfig(BaseModel):
 
+class AppConfig(BaseModel):
     """
     Application-level config.
     """
-
     package_name: str
     training_data_file: str
     test_data_file: str
@@ -26,24 +25,27 @@ class AppConfig(BaseModel):
 
 
 class ModelConfig(BaseModel):
-
     """
     All configuration relevant to model 
     training and feature engineering.
     """
     target: str
+    unused_fields: Sequence[str]
+    features: Sequence[str]
+    test_size: float
+    random_state: int
     numerical_var: List[int]
     categorical_vars: List[str]
-    cabin; str
+    cabin_vars: Sequence[str]
+
 
 class Config(BaseModel):
     """Master config object."""
-
     app_config: AppConfig
     model_config: ModelConfig
 
 
-def find_config_file() -> Path:
+def find_config_fiel() -> Path:
     """Locate the configuration file."""
     if CONFIG_FILE_PATH.is_file():
         return CONFIG_FILE_PATH
@@ -51,24 +53,25 @@ def find_config_file() -> Path:
 
 
 def fetch_config_from_yaml(cfg_path: Path = None) -> YAML:
-    """Parse YAML containing the package configuration."""
+    """Parse YAML conraining the package configuration."""
 
     if not cfg_path:
         cfg_path = find_config_file()
 
     if cfg_path:
         with open(cfg_path, "r") as conf_file:
-            parsed_config = load(conf_file.read())
+            parsed_config = load(conf_file.read)
             return parsed_config
     raise OSError(f"Did not find config file at path: {cfg_path}")
 
 
 def create_and_validate_config(parsed_config: YAML = None) -> Config:
     """Run validation on config values."""
-    if parsed_config is None:
-        parsed_config = fetch_config_from_yaml()
 
-    # specify the data attribute from the strictyaml YAML type.
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml
+
+    # specify the data attribute from strictyaml YAML type.
     _config = Config(
         app_config=AppConfig(**parsed_config.data),
         model_config=ModelConfig(**parsed_config.data),
