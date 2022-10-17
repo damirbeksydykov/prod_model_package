f39756530f61baf603e5000b0438206adef0c795
damirbekydykov <damirbek.sydykov@yahoo.de>
2022-10-18 01:00:55 +0200

    updated requirements file

--- classification_model/pipeline.py
+++ classification_model/pipeline.py
@@ -1,8 +1,13 @@
 # for encoding categorical variables
 from feature_engine.encoding import OneHotEncoder, RareLabelEncoder
 
+
 # for imputation
-from feature_engine.imputation import AddMissingIndicator, CategoricalImputer, MeanMedianImputer
+from feature_engine.imputation import (
+    AddMissingIndicator,
+    CategoricalImputer,
+    MeanMedianImputer,
+)
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 # Package meta-data
-NAME = 'tid-titanic-classification-model'
+NAME = "tid-titanic-classification-model"
 DESCRIPTION = "Example Titanic dataset classification model package from Train In Data."
 URL = "https://github.com/damirbeksydykov/prod_model_package"
 EMAIL = "damirbek.sydykov@yahoo.de"
@@ -18,16 +18,17 @@ long_description = DESCRIPTION
 # Load the package's VERSION file as a dictionary.
 about = {}
 ROOT_DIR = Path(__file__).resolve().parent
-REQUIREMENTS_DIR = ROOT_DIR / 'requirements'
-PACKAGE_DIR = ROOT_DIR / 'classification_model'
+REQUIREMENTS_DIR = ROOT_DIR / "requirements"
+PACKAGE_DIR = ROOT_DIR / "classification_model"
 with open(PACKAGE_DIR / "VERSION") as f:
     _version = f.read().strip()
     about["__version__"] = _version
 
 # What packages are required for this module to be executed?
 def list_reqs(fname="requirements.txt"):
-    with open(REQUIREMENTS_DIR /fname) as fd:
+    with open(REQUIREMENTS_DIR / fname) as fd:
         return fd.read().splitlines()
+
 
 # Setup
 setup(
